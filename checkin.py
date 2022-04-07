3c2c4050b908c4c61ac3831db2db9174a5817426
hlc <beibo.huang@tuya.com>
2022-04-07 11:33:03 +0800

    commit

--- checkin.py
+++ checkin.py
@@ -8,7 +8,7 @@ def str_to_dict(str):
     '''
     dict = {}
     for i in str.split(';'):
-        dict[i.split('=')[0]] = i.split('=')[1]
+        dict[i.split('=')[0].strip()] = i.split('=')[1].strip()
     return dict
 
 cookies = ver_info_str = os.environ.get('COOKIES')

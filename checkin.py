74948ba1cfcdae6f7f2702042842da1a21886137
hlc <beibo.huang@tuya.com>
2022-04-07 11:32:05 +0800

    commit

--- checkin.py
+++ checkin.py
@@ -1,5 +1,15 @@
 import os
 import requests
 
+
+def str_to_dict(str):
+    '''
+    将字符串转换为字典
+    '''
+    dict = {}
+    for i in str.split(';'):
+        dict[i.split('=')[0]] = i.split('=')[1]
+    return dict
+
 cookies = ver_info_str = os.environ.get('COOKIES')
-print("cookies:", cookies.replace("=", ":"))
+print("cookies:", str_to_dict(cookies))

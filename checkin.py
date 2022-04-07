e520627499c107140f11905453937a085b95335f
hlc <beibo.huang@tuya.com>
2022-04-07 11:55:18 +0800

    commit

--- checkin.py
+++ checkin.py
@@ -2,6 +2,7 @@ import os
 import requests
 
 
+
 def str_to_dict(str):
     '''
     将字符串转换为字典
@@ -15,11 +16,13 @@ cookies = os.environ.get('COOKIES')
 cookies = str_to_dict(cookies)
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0"
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55"
 }
 print("cookies:", cookies)
 print("headers:", headers)
-response = requests.post("https://www.wanbianios.com/wp-admin/admin-ajax.php", data="action=user_qiandao", cookies=cookies, headers=headers)
+response = requests.post("https://www.wanbianios.com/wp-admin/admin-ajax.php", data="action=user_qiandao", headers=headers, cookies=cookies)
 response.encoding = 'utf-8'
 print(response.status_code)
-print("response:", response.json)
+print("response:", response.json())
+print("response.text:", response.text)

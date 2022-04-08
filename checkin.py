29f60b67ca879a9914dedbf0010d397e3df7befa
hlc <beibo.huang@tuya.com>
2022-04-08 10:33:16 +0800

    commit

--- checkin.py
+++ checkin.py
@@ -1,6 +1,10 @@
 import os
 import requests
 
+def send_plus_msg(msg):
+    url = f"http://www.pushplus.plus/send?token=78465b494d2d494a905bfa5ff02444d2&title={msg}&content={msg}&template=html"
+    requests.get(url)
+
 def str_to_dict(str):
     '''
     将字符串转换为字典
@@ -22,3 +26,12 @@ response = requests.post("https://www.wanbianios.com/wp-admin/admin-ajax.php", d
 response.encoding = 'utf-8'
 print("status_code:", response.status_code)
 print("response:", response.json())
+
+if str(response.text) == "0":
+    print("错误，请检查COOKIES")
+    send_plus_msg("签到错误，请检查COOKIES，返回值：" + str(response.text))
+elif response.json()['status'] == 1:
+    print("签到成功")
+    send_plus_msg("签到成功")
+elif response.json()['status'] == 0:
+    print("今日已签到")
